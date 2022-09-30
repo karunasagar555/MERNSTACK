8f31e2198b7b4a3f049c3fb15f3b2b4f6769a444
karunasagar555 <skarunasagar3@gmail.com>
2022-09-30 20:33:21 +0530

    :fire: ReactInterviewPrep, ReactNotes, Reacterrors,Reactshortcutcommands added

---
+++ ShortcutCommands.jsx
@@ -0,0 +1,13 @@
+//1)) rcc -> React Class Components
+
+import React, { Component } from 'react'
+
+export default class ShortcutCommands extends Component {
+  render() {
+    return (
+      <div>ShortcutCommands</div>
+    )
+  }
+}
+
+
